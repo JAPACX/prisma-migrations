332ab7c50e891a25a88fae82ea3007a8298d9cef
japacx <jepacheco98@gmail.com>
2023-11-13 18:55:07 -0500

    fix: update videos and topics in the same seed

--- prisma/seeds/TechnicalAdvice.ts
+++ prisma/seeds/TechnicalAdvice.ts
@@ -2,23 +2,42 @@ import { PrismaClient } from "@prisma/client";
 import videos from "../../data/technicalAdvice/output.json";
 
 const prisma = new PrismaClient();
+
 export default async function VideosSeed() {
   for (let index = 0; index < videos.length; index++) {
-    await prisma.topic.create({
-      data: {
-        id: videos[index].id,
-        name: videos[index].name,
-        videos: {
-          create: videos[index].videos.map((element) => {
-            return {
-              id: element.id,
-              description: element.description,
-              time: element.time,
-              url: element.url,
-            };
-          }),
-        },
+    const videoData = videos[index];
+
+    // Actualizar el elemento padre (topic) si es necesario
+    await prisma.topic.upsert({
+      where: { id: videoData.id },
+      update: {
+        name: videoData.name,
+      },
+      create: {
+        id: videoData.id,
+        name: videoData.name,
       },
     });
+
+    // Actualizar los elementos hijos (videos) siempre
+    for (const element of videoData.videos) {
+      await prisma.videos.upsert({
+        where: { id: element.id },
+        update: {
+          description: element.description,
+          time: element.time,
+          url: element.url,
+        },
+        create: {
+          id: element.id,
+          description: element.description,
+          time: element.time,
+          url: element.url,
+          topic: {
+            connect: { id: videoData.id },
+          },
+        },
+      });
+    }
   }
 }
